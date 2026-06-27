8c2830e56106f63850307d7d4a3474186f7c67b7
Nyamthabad Lingoji <nyamthabad@gmail.com>
2026-06-27 12:03:01 +0530

    Update day4 exercises

--- day4.py
+++ day4.py
@@ -1,28 +1,28 @@
 
-# 1 using a loop  
+1 using a loop  
 
-# magicians = ['alice', 'david', 'carolina']
-# for magician in magicians:
-#  print(magician.title() + ", that was a great trick!") 
-
-
-# 2 add more items to a loop 
-# magicians = ['alice', 'david', 'carolina']
-# for magician in magicians:
-#  print(magician.title() + ", that was a great trick!")
-#  print("I can't wait to see your next trick, " + magician.title() + ".\n")
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+ print(magician.title() + ", that was a great trick!") 
 
 
-# #  using range () skipping the functions
-# even_numbers = list(range(2,11,2))
-# print(even_numbers)
+2 add more items to a loop 
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+ print(magician.title() + ", that was a great trick!")
+ print("I can't wait to see your next trick, " + magician.title() + ".\n")
 
-# #  first 10 numberes squares rots
-# squares = []
-# for value in range(1,11):
-#     square = value**2
-#     squares.append(square)
-# print(squares)
+
+#  using range () skipping the functions
+even_numbers = list(range(2,11,2))
+print(even_numbers)
+
+#  first 10 numberes squares rots
+squares = []
+for value in range(1,11):
+    square = value**2
+    squares.append(square)
+print(squares)
 
 
 # 3
