e375170bcf9c4c32fe727f6890757124e6b1dd3f
Nyamthabad Lingoji <nyamthabad@gmail.com>
2026-06-27 10:25:55 +0530

    Add day3.py

---
+++ day3.py
@@ -0,0 +1,18 @@
+# using the range()function
+# for value in range(1,10):
+#     print(value)
+
+# using range()to make a list of numbers
+# numbers = list(range(1,6))
+# print(numbers)
+
+# using range printing the odd numbers
+odd_numbers = list(range(2,12,2))
+print(odd_numbers)
+
+#  squares with using the range() funtions
+squares  = []
+for value in range(1,11):
+   square = value**2
+   squares.append(square)
+print(squares)
