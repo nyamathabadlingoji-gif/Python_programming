6f572e493af9428d9b4effa576dfe2395da122b3
Nyamthabad Lingoji <nyamthabadlingoji@example.com>
2026-07-07 20:36:11 +0530

    Add dictionary module and Python files

---
+++ dictionary/simpledic.py
@@ -0,0 +1,45 @@
+
+# This is a simple dictionary in Python. A dictionary is a collection of key-value pairs, where each key is unique and maps to a specific value. In this example, the dictionary contains three key-value pairs: "name" maps to "john", "age" maps to 30, and "course" maps to "python". The `print(dict)` statement will output the entire dictionary when the code is executed.
+
+dict =  {
+    "name": "john",
+    "age": 30,
+    "course": "python"
+}
+print(dict)
+
+# dictionary with five students and their marks
+marks = {
+    "Alice": 85,
+    "Bob": 92,
+    "Charlie": 78,
+    "David": 90,
+    "Eva": 95
+}
+print(marks)
+
+# dictionary with five countries and their capitals
+capitals = {
+    "USA": "Washington, D.C.",
+    "Canada": "Ottawa",
+    "Germany": "Berlin",
+    "France": "Paris",
+    "Japan": "Tokyo"
+}
+print(capitals)
+
+# dictionary to store details of three students.
+students = {
+    "student1":{
+        "name":"lingan",
+        "age":20,
+        "course":"dl"
+    },
+    "student2":{
+        "name":"sai",
+        "age":21,
+        "course":"ml"
+    }
+
+}
+print(students)
