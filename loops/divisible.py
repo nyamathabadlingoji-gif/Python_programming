dfd7017d67b66003e36618bf4a810b65d462fe70
Nyamthabad Lingoji <nyamthabadlingoji@example.com>
2026-07-08 23:32:27 +0530

    Add pattern printing exercises to loops/divisible.py

--- loops/divisible.py
+++ loops/divisible.py
@@ -28,3 +28,55 @@ for char in text:
     if char in vowels:
         count += 1
 print("Number of vowels in the string:", count)
+
+
+# 1. Print
+# *
+# **
+# ***
+# ****
+# *****
+for i in range(1,6):
+    print("*" * i)
+
+
+# print
+# *****
+# ****
+# ***
+# **
+# *
+for i in range(5,0,-1):
+    print("*" * i)
+
+# 3. Print
+# 1
+# 12
+# 123
+# 1234
+# 12345
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j, end="")
+    print()
+
+# 4. Print
+# 11111
+# 22222
+# 33333
+# 44444
+# 55555
+for i in range(1, 6):
+    for j in range(5):
+        print(i, end="")
+    print()
+
+# 5. Print Multiplication Tables from 1 to 5
+for i in range(1, 6):
+    print(f"Multiplication Table of {i}")
+
+    for j in range(1, 11):
+        print(f"{i} x {j} = {i * j}")
+
+    print()
